a00fa106b70aba3cedb72173db652c6c48fdee88
Victor Arana <victor@codersfree.com>
2025-06-13 18:38:48 -0500

    actualizacion licencia

--- config/theme.php
+++ config/theme.php
@@ -4,10 +4,9 @@ return [
 
     'default' => env('THEME_DEFAULT', 'default'),
 
-    'active' => env('THEME_ACTIVE', 'default'),
-
     'paths' => [
         'views' => env('THEME_VIEWS_PATH', resource_path('themes')),
+        //Los assets deben estar en public para que sean accesibles desde el navegador
         'assets' => env('THEME_ASSETS_PATH', public_path('themes')),
         'stubs' => env('THEME_STUBS_PATH', resource_path('themes/stubs')),
     ]
